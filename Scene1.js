e2e07bb134235225794fcbd367811cd2622c4156
GhostieeKoto <33725354+GhostieeKoto@users.noreply.github.com>
2023-10-21 18:55:43 +0000

    Fix Font

--- Scene1.js
+++ Scene1.js
@@ -3,7 +3,7 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
     create() {
-        this.add.text(20, 20, "Loading game...", {font: '25px Comic sans', fill: 'red'});
+        this.add.text(20, 20, "Loading game...", {font: '25px Arial', fill: 'red'});
         this.scene.start("playGame");
     }
 
