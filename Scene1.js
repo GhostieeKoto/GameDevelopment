c5dd7339ec52005b41713c5e702a757d348c5f80
bfrikken-rcp <bfrikken@revcycle-partners.com>
2023-10-21 14:55:26 -0400

    scene 1 development

--- Scene1.js
+++ Scene1.js
@@ -3,7 +3,8 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
     create() {
-        this.add.text(20, 20, "Loading game...", {font: '25px Comic sans', fill: 'red'});
+        this.add.text(20, 20, "Loading game...", {font: '25px Arial', fill: 'red'});
+        setTimeout(5000, )
         this.scene.start("playGame");
     }
 
--- Scene2.js
+++ Scene2.js
@@ -2,5 +2,10 @@ class Scene2 extends Phaser.Scene {
     constructor() {
         super("playGame");
 
+
+    }
+
+    create(){
+        this.add.text(20,20,"Playing game", {font: '25px Comic sans', fill: 'green' });
     }
 }
