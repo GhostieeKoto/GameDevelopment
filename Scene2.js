75a2fd68cef81b1e7aa9c82b171c494d06b20c64
GhostieeKoto <33725354+GhostieeKoto@users.noreply.github.com>
2023-10-21 19:43:40 +0000

    scene 2

--- Scene2.js
+++ Scene2.js
@@ -9,8 +9,8 @@ class Scene2 extends Phaser.Scene {
         this.add.text(20,20,"Playing game", {font: '25px Comic sans', fill: 'green' });
         this.background = this.add.image(0, 0, "background");
         this.background.setOrigin(0,0);
-        this.player = this.add.image(192, 108, "player");
+        this.player = this.add.image(0, 0, "player");
         this.player.setOrigin(0,0);
-        this.player.setScale(2);
+        this.player.setScale(3, 3);
     }
 }
