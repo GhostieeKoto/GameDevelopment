e5c9692edff63b06cb88bfd4916473224fb3179d
GhostieeKoto <33725354+GhostieeKoto@users.noreply.github.com>
2023-10-21 18:53:53 +0000

    Edit Text

--- Scene1.js
+++ Scene1.js
@@ -3,7 +3,7 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
     create() {
-        this.add.text(20, 20, "Loading game...");
+        this.add.text(20, 20, "Loading game...", {font: '25px Comic sans', fill: 'red'});
         this.scene.start("playGame");
     }
 
