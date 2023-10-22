8ad46aae1ac6847e1e0b85e0426b6664b5d0a206
GhostieeKoto <33725354+GhostieeKoto@users.noreply.github.com>
2023-10-22 13:01:30 +0000

    e

--- Scene2.js
+++ Scene2.js
@@ -8,6 +8,7 @@ class Scene2 extends Phaser.Scene {
     tick = 0;
 
     create(){
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
         this.add.text(20,20,"Playing game", {font: '25px Comic sans', fill: 'green' });
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0,0);
